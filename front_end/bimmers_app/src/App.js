215e7f8a258ca96d1bcc0751f224270b580ffe3e
Mohammad Alsadoon <mnafnm@umsystem.edu>
2023-11-07 23:30:29 -0600

    main

--- front_end/bimmers_app/src/App.js
+++ front_end/bimmers_app/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './views/Navbar';
 import Home from './views/Home';
+import Check_in from './views/Check_in';
+import Repair_order from './views/Repair_order';
+import Invoices from './views/Invoices';
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <Home/>
-  );
+   <>
+    <Navbar/>
+    <div className='container'>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/check_in" element={<Check_in/>}/>
+        <Route path="/repair_order" element={<Repair_order/>}/>
+        <Route path="/invoices" element={<Invoices/>}/>
+        
+      </Routes>
+    </div>
+   </>
+  )
 }
 
 export default App;
