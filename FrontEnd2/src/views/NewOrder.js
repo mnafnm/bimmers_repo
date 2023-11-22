8629c3e9c7539e62c47083d3b393668f80894971
Ilikepizza2 <lit2021010@iiitl.ac.in>
2023-11-23 01:19:23 +0530

    added previous orders table

---
+++ FrontEnd2/src/views/NewOrder.js
@@ -0,0 +1,206 @@
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import firebaseapp from '../utils/initfirebase'
+import logo from '../assets/logo.png'
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
+import { Toast } from '@chakra-ui/react';
+const uniqueId = Math.random().toString(36).substr(2, 9);
+const NewOrder = () => {
+    const [loading, setLoading] = React.useState(false)
+    const date = new Date();
+    const [descriptions, setDescriptions] = React.useState([])
+    // console.log(data)
+    const [data, setData] = React.useState({});
+    const navigate = useNavigate()
+    const handleClick = (data, ind) => {
+        navigate(`/RepairOrder?ind=${ind}`, { state: data })
+
+    }
+    useEffect(() => {
+        const db = getFirestore(firebaseapp);
+        const id = new URLSearchParams(window.location.search).get('id')
+        const docRef = doc(db, "customers", id);
+        const getData = async () => {
+            const docSnap = await getDoc(docRef);
+            setData(docSnap.data())
+        }
+        getData()
+    }, [])
+    console.log(data)
+    const clickHandler = () => {
+        setDescriptions([
+            ...descriptions,
+            ""
+        ])
+    }
+    
+    const saveHandler = async () => {
+        setLoading(true)
+        const db = getFirestore(firebaseapp);
+        data.Orders.push({
+            Descriptions: descriptions
+        })
+        try{
+            await setDoc(doc(db, "customers", data.Id), data, { merge: true })
+            Toast({
+                title: "Order Created.",
+                description: "We've created your order for you.",
+                status: "success",
+                duration: 9000,
+                isClosable: true,
+            })
+        }
+        catch(e){
+            console.log(e)
+        }
+        handleClick(data, data.Orders.length - 1)
+        setLoading(false)
+        // window.print();
+      }
+    
+    return (
+
+        <div class="max-w-5xl mx-auto py-16 bg-white">
+            <article class="overflow-hidden">
+                <div class="bg-[white] rounded-b-md">
+                    <div class="p-9">
+                        <div class="space-y-6 text-slate-700">
+                            <img class="object-cover h-12" src={logo} alt="" />
+
+                            <p class="text-xl font-extrabold tracking-tight uppercase font-body">
+                                Bimmers R Us
+                            </p>
+                        </div>
+                    </div>
+                    <div class="p-9">
+                        <div class="flex w-full">
+                            <div class="grid grid-cols-4 gap-12">
+                                <div class="text-sm font-light text-slate-500">
+                                    <p class="text-sm font-normal text-slate-700">
+                                        Invoice Detail:
+                                    </p>
+                                    <p>{data.CusFname} {data.CusLname}</p>
+                                    <p>{data.Street} {data.City} {data.StateName}</p>
+                                    <p>{data.ZipCode}</p>
+                                    <p>{data.PhoneNum}</p>
+                                </div>
+                                <div class="text-sm font-light text-slate-500">
+                                    <p class="text-sm font-normal text-slate-700">Billed To</p>
+                                    <p>Bimmers R Us</p>
+                                </div>
+                                <div class="text-sm font-light text-slate-500">
+                                    <p class="text-sm font-normal text-slate-700">Invoice Number</p>
+                                    <p>{uniqueId}</p>
+
+                                    <p class="mt-2 text-sm font-normal text-slate-700">
+                                        Date of Issue
+                                    </p>
+                                    <p>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="flex flex-col">
+                        <div class="overflow-x-auto md:-mx-8">
+                            <div class="inline-block min-w-full py-2 sm:px-6 md:px-8">
+                                <div class="overflow-hidden">
+                                    <table class="min-w-full text-center text-sm font-light">
+                                        <thead
+                                            class="border-b bg-neutral-800 font-small text-white dark:border-neutral-500 dark:bg-neutral-900">
+                                            <tr>
+                                                <th scope="col" class=" px-6 py-4">Colour</th>
+                                                <th scope="col" class=" px-6 py-4">Year</th>
+                                                <th scope="col" class=" px-6 py-4">Care Make</th>
+                                                <th scope="col" class=" px-6 py-4">Car Model</th>
+                                                <th scope="col" class=" px-6 py-4">Licence</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="border-b dark:border-neutral-500">
+                                                <td class="whitespace-nowrap  px-6 py-4">{data.CarColor}</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">{data.CarYear}</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">{data.CarMake}</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">{data.CarModel}</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">{data.PlateNum}</td>
+                                            </tr>
+
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className="relative p-6 flex-auto">
+                        <div className="flex-col">
+                            {descriptions.length > 0 ? descriptions.map((description, index) => (
+                                <div className="flex-col flex gap-2" key={index}>
+                                    <div className='flex justify-between'>
+                                        <div className="text-left">Order Description {index + 1}:</div>
+                                        <div className='bg-slate-500 px-3 py-1 rounded-md text-white hover:bg-slate-700 cursor-pointer' onClick={()=>setDescriptions((prev)=>prev.filter((_, i) => i !== index))}>X</div>
+                                    </div>
+                                    <textarea className="border-2 rounded-md shadow-sm p-3" value={descriptions[index]} placeholder={`Description #${index + 1}`} onChange={(e) => setDescriptions((prev) => prev.map((c, i) => i == index ? e.target.value : c))} />
+                                </div>
+                            )) : <div>No Descriptions.</div>}
+                            <div className="flex justify-end">
+                                <div onClick={clickHandler} className="bg-slate-400 rounded-md cursor-pointer hover:bg-slate-600 py-2 px-4 text-white font-medium w-fit">Add Description</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-9">
+                        <div class="flex flex-col mx-0 mt-8 text-sm font-normal text-slate-700">
+                            Technician notes:
+                            <div className='flex-col gap-10 flex pt-16'>
+                                <div className='w-full border-slate-500 border'></div>
+                                <div className='w-full border-slate-500 border'></div>
+                                <div className='w-full border-slate-500 border'></div>
+                                <div className='w-full border-slate-500 border'></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-end items-end px-7 gap-5'>
+                        <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.print()}  >
+                            Print
+                        </button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>saveHandler()} >
+                            {!loading?"Create repair order":"Loading..."}
+                        </button>
+                    </div>
+
+
+
+                    <div class="mt-48 p-9">
+                        <div class="border-t pt-9 border-slate-200">
+                            <div class="text-sm font-light text-slate-700">
+                                <p>
+                                    Payment terms are 14 days. Please be aware that according to the
+                                    Late Payment of Unwrapped Debts Act 0000, mechanics are
+                                    entitled to claim a 00.00 late fee upon non-payment of debts
+                                    after this time, at which point a new invoice will be submitted
+                                    with the addition of this fee. If payment of the revised invoice
+                                    is not received within a further 14 days, additional interest
+                                    will be charged to the overdue account and a statutory rate of
+                                    8% plus Bank of US base of 0.5%, totalling 8.5%. Parties
+                                    cannot contract out of the Act’s provisions.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+
+    );
+}
+
+export default NewOrder
---
+++ FrontEnd2/src/views/Orders.js
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from 'react'
+import firebaseapp from '../utils/initfirebase'
+import { getFirestore, collection, getDocs, deleteDoc, doc, setDoc } from "firebase/firestore";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    useReactTable,
+} from '@tanstack/react-table'
+import Modal from '../components/modal';
+import { useNavigate } from 'react-router-dom';
+const columnHelper = createColumnHelper()
+
+const handleDelete = (data) => {
+    const db = getFirestore(firebaseapp);
+    console.log(data.Id)
+    const deleteData = async () => {
+        try {
+            await deleteDoc(doc(db, "customers", data.Id))
+            window.location.reload()
+        }
+        catch (e) {
+            console.log("Some Error Occured", e)
+        }
+    }
+    deleteData()
+}
+
+const Orders = () => {
+    const columns = [
+        columnHelper.accessor('CusFname', {
+            header: () => <span>First Name</span>,
+            cell: info => info.getValue(),
+        }),
+        columnHelper.accessor('CusLname', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Last Name</span>,
+        }),
+        columnHelper.accessor('CarModel', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Car Model</span>,
+        }),
+        columnHelper.accessor('CarMake', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Car Make</span>,
+        }),
+        columnHelper.accessor('CarYear', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Car Year</span>,
+        }),
+        columnHelper.accessor('CarColor', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Car Color</span>,
+        }),
+        columnHelper.accessor('PlateNum', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Plate Number</span>,
+        }),
+        columnHelper.accessor('PhoneNu', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Phone Number</span>,
+        }),
+        columnHelper.accessor('Street', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Street</span>,
+        }),
+        columnHelper.accessor('City', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>City</span>,
+        }),
+        columnHelper.accessor('StateName', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>State</span>,
+        }),
+        columnHelper.accessor('ZipCode', {
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Zip Code</span>,
+        }),
+        columnHelper.display({
+            id: "Description",
+            cell: info =>
+                <>
+                    <Modal data={info.row.original} saveHandler={saveHandler} setData={setData} />
+                </>,
+            header: "Orders",
+        }),
+    ]
+
+    const navigate = useNavigate()
+    const handleClick = (props) => {
+        // console.log("Hello", dataToSend)
+        toast.success("Data Saved Successfully")
+        navigate("/RepairOrder", { state: props })
+
+    }
+    const saveHandler = async (id, descriptions, row) => {
+        const db = getFirestore(firebaseapp);
+        // row.Orders descriptions
+        await setDoc(doc(db, "customers", id), row, { merge: true })
+        // window.print();
+    }
+
+
+    const [data, setData] = React.useState([])
+
+
+
+
+
+
+
+    const [columnFilters, setColumnFilters] = useState('');
+    const [loading, setLoading] = useState(false);
+    console.log(loading)
+    useEffect(() => {
+        setLoading(true)
+        const db = getFirestore(firebaseapp);
+        const docRef = collection(db, "customers");
+        const getData = async () => {
+            const data = await getDocs(docRef);
+            setData(data.docs.map(doc => doc.data()))
+        }
+        getData()
+        setLoading(false)
+    }, [])
+    console.log(data)
+
+    const table = useReactTable({
+        data,
+        columns,
+        state: {
+            globalFilter: columnFilters,
+        },
+        onGlobalFilterChange: setColumnFilters,
+        getCoreRowModel: getCoreRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+    })
+
+    // console.log(data)
+
+
+    return (
+        <>
+
+            <div class="flex flex-col pt-5">
+
+                <div class='w-[40vw] mx-auto'>
+                    <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+                        <div class="grid place-items-center h-full w-12 text-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                        </div>
+
+                        <input
+                            class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+                            type="text"
+                            id="search"
+                            placeholder="Search something.."
+                            value={columnFilters}
+                            onChange={e => setColumnFilters(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+                <div class="pt-5 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                        <div class="overflow-auto max-h-[80vh]">
+                            <table class="min-w-full text-center text-sm font-light">
+                                <thead
+                                    class="border-b bg-neutral-800 font-medium text-white dark:border-neutral-500 dark:bg-neutral-900">
+                                    {/* <tr> */}
+                                    {table.getHeaderGroups().map(headerGroup => (
+                                        <tr key={headerGroup.id}>
+                                            {headerGroup.headers.map(header => (
+                                                <th key={header.id}>
+                                                    {header.isPlaceholder
+                                                        ? null
+                                                        : flexRender(
+                                                            header.column.columnDef.header,
+                                                            header.getContext()
+                                                        )}
+                                                </th>
+                                            ))}
+                                        </tr>
+                                    ))}
+
+                                </thead>
+                                <tbody>
+                                    {loading ? <div className='text-center p-5 animate-pulse'>loading</div> : table.getRowModel().rows.map(row => (
+                                        <tr key={row.id}>
+                                            {row.getVisibleCells().map(cell => (
+                                                <td key={cell.id}>
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </td>
+                                            ))}
+                                        </tr>
+                                    ))}
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="h-4" />
+        </>
+    )
+}
+
+export default Orders
